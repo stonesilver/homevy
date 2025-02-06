805ddf6a0a6e86f71a4d62b9f65247dfe6db8e05
stonesilver <stonesilver12@gmail.com>
2025-02-06 07:21:15 +0100

    (fix): changed @svgr/webpack config

--- next.config.ts
+++ next.config.ts
@@ -13,11 +13,24 @@ const nextConfig: NextConfig = {
     },
   },
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.(".svg"));
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
   },
