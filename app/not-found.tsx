cd39c0cb0d4b0d42adf447ff8073ba9b655062e9
stonesilver <stonesilver12@gmail.com>
2025-02-06 08:21:53 +0100

    (chore): replace 404.svg file with webp image
    - Deleted 404.svg file because the file size is too big and replaced it with an optimized webp image

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
-import NotFoundIcon from "@/assets/icons/404.svg";
+import NotFoundIcon from "@/public/img/ghost-404.webp";
+import Image from "next/image";
 
 const NotFound = () => {
   return (
     <div className="content-layout center-item h-dvh flex-col py-[6.25rem]">
       <div className="center-item flex-1 flex-col">
-        <NotFoundIcon className="max-md:scale-[0.7]" />
+        <Image src={NotFoundIcon} alt="page not found" className="block h-[200px] object-contain" />
         <h1 className="mt-4 text-center text-2xl font-medium text-primary sm:text-4xl">Boo! Page missing!</h1>
         <p className="mx-auto mt-2 max-w-[472px] text-center text-sm text-gray-main sm:text-base">
           It&lsquo;s gone... vanished like a phantom. But don&lsquo;t worry, the site&lsquo;s still full of treats!
